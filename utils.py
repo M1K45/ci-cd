444a1eca9894c5f272460ff387e95fac47d9438c
M1K45 <272570@student.pwr.edu.pl>
2025-05-20 15:53:03 +0200

    Adding docstrings and final newline

--- utils.py
+++ utils.py
@@ -1,11 +1,27 @@
+'''
+Set of bacis math functions
+'''
+
 def add ( a : int , b : int ) -> int :
+    '''
+    Adding function
+    '''
     return a + b
 
 def subtract ( a : int , b : int ) -> int :
+    '''
+    Subtracting function
+    '''
     return a - b
 
 def multiply ( a : int , b : int ) -> int :
+    '''
+    Multiplying two integers
+    '''
     return a * b
 
 def divide ( a : int , b : int ) -> float :
+    '''
+    Dividing integers -> float
+    '''
     return a / b
